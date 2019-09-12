274dbe7367a83c3541a290c9e45f87aae86f3be7
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-09-12 16:57:13 -0300

    Adição do triceratops

---
+++ Assets/RandomAnimBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimBehaviour : StateMachineBehaviour{
+
+    public int numAnimacoes;
+
+    override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
+        animator.SetInteger("valor", Random.Range(0, (numAnimacoes-1)));
+    }
+
+}
