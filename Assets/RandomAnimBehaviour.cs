94275ac6a7a914849e7b2f98f7e3426d59a6ed4d
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-09-13 03:19:59 -0300

    Controlador de animacoes aleatorias ajustado

--- Assets/RandomAnimBehaviour.cs
+++ Assets/RandomAnimBehaviour.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomAnimBehaviour : StateMachineBehaviour{
+public class RandomAnimBehaviour : StateMachineBehaviour {
 
     public int numAnimacoes;
 
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
-        animator.SetInteger("valor", Random.Range(0, (numAnimacoes-1)));
+        int valor = Random.Range(0, numAnimacoes);
+        if (animator.name == "tiranosauro rex") {
+            Debug.Log(valor);
+        }
+        animator.SetInteger("valor", valor);
     }
 
 }
