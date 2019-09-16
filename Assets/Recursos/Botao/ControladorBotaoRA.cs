298552322b7b17cd1954e08292c053279748bd08
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-09-15 22:38:44 -0300

    Adição da base de dados com uma carta exemplo e botão virtual vuforia

---
+++ Assets/Recursos/Botao/ControladorBotaoRA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ControladorBotaoRA : MonoBehaviour, IVirtualButtonEventHandler{
+
+    public GameObject[] objetos;
+    public GameObject vuforiaVirtualButton;
+    public GameObject botao3D;
+
+    private int index = 0;
+
+    void Start() {
+        vuforiaVirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        objetos[index].SetActive(false);
+        index = (index == (objetos.Length-1)) ? 0 : (index + 1);
+        objetos[index].SetActive(true);
+        botao3D.GetComponent<Animator>().Play("pressionar");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        botao3D.GetComponent<Animator>().Play("soltar");
+    }
+}
