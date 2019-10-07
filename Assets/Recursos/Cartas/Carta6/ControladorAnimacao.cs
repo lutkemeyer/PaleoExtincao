1bb462c6c85d130ad02441745bb7a195ab4e606e
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-06 23:02:46 -0300

    Adição das cartas, e algumas modelagens

---
+++ Assets/Recursos/Cartas/Carta6/ControladorAnimacao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorAnimacao : MonoBehaviour{
+    public enum TipoReproducao{ SEQUENCIAL = 0, ALEATORIO = 1 }
+
+    public TipoReproducao tipoReproducao;
+
+    public Motion[] animacoes;
+
+    void Start(){
+
+        Animator animator = GetComponent<Animator>();
+        
+    }
+
+    void Update(){
+        
+    }
+}
