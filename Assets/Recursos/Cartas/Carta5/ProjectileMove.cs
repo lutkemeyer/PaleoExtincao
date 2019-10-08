fb9d9b4ffc45b4e8e19a7a4e4af9414366074a2e
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-08 01:12:53 -0300

    Meteoro adicionado a carta 3

---
+++ Assets/Recursos/Cartas/Carta5/ProjectileMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour{
+
+    public float speed;
+    public GameObject impactPrefab;
+
+    private Rigidbody rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate(){
+        if (speed != 0 && rb != null) {
+            rb.position += transform.forward * (speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Recursos/Cartas/Carta5/SpawnMeteor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMeteor : MonoBehaviour{
+
+    public GameObject vfx;
+    public Transform startPoint;
+    public Transform endPoint;
+
+    void Start(){
+        Vector3 startPos = startPoint.position;
+        GameObject objVFX = Instantiate(vfx, startPos, Quaternion.identity) as GameObject;
+        Vector3 endPos = endPoint.position;
+        RotateTo(objVFX, endPos);
+    }
+
+    void RotateTo(GameObject obj, Vector3 destination){
+        Vector3 direction = destination - obj.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
