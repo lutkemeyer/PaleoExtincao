9c7ee7be5e57377ca901609081e25cfcbc5f6d60
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-08 07:24:59 -0300

    Meteoro melhorado

--- Assets/Recursos/Cartas/Carta3/InstanciadorMeteoro.cs
+++ Assets/Recursos/Cartas/Carta3/InstanciadorMeteoro.cs
@@ -11,6 +11,8 @@ public class InstanciadorMeteoro : MonoBehaviour{
 
     private float tempoRestante;
 
+    private GameObject meteoroAtual;
+
     void Start(){
         Spawn();
         tempoRestante = tempoDeSpawn;
@@ -18,9 +20,9 @@ public class InstanciadorMeteoro : MonoBehaviour{
 
     private void Spawn(){
         Vector3 posicaoInicial = new Vector3(0, ((float)distanciaMeteoro)/10, 0);
-        GameObject meteoroInstanciado = Instantiate(meteoro, posicaoInicial, Quaternion.identity) as GameObject;
-        meteoroInstanciado.transform.SetParent(pontoFinal, false); // setando o meteoro como filho de PontoFinal
-        
+        meteoroAtual = Instantiate(meteoro, posicaoInicial, Quaternion.identity) as GameObject;
+        meteoroAtual.transform.SetParent(pontoFinal, false); // setando o meteoro como filho de PontoFinal
+        //Debug.Log(posicaoInicial);
         //Vector3 posicaoFinal = pontoFinal.transform.position;
         //Vector3 direction = posicaoFinal - meteoroInstanciado.transform.position;
         //Quaternion rotation = Quaternion.LookRotation(direction);
@@ -29,11 +31,13 @@ public class InstanciadorMeteoro : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-
+        if (meteoroAtual == null || meteoroAtual.Equals(null)) {
+            Spawn();
+        }
         tempoRestante -= Time.deltaTime;
         if (tempoRestante < 0) {
             tempoRestante = tempoDeSpawn;
-            Spawn();
+            //Spawn();
         }
     }
 }
---
+++ Assets/Recursos/Cartas/Carta3/MovimentoMeteoroInfinito.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoMeteoroInfinito : MonoBehaviour{
+
+    public float velocidade;
+    public GameObject impactPrefab;
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+    }
+
+    void FixedUpdate() {
+        if (velocidade != 0 && rb != null) {
+            rb.position += (transform.up * -1) * (velocidade * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        velocidade = 0;
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        if (impactPrefab != null) {
+
+        }
+        Debug.Log("colidiu");
+        Destroy(gameObject);
+    }
+}
