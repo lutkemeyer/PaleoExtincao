65c33c795bfb3e09d20db3fd51d68c7094d3e76a
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-10 11:58:05 -0300

    Fumaça global adicionada

--- Assets/Recursos/Cartas/Carta3/Scripts/InstanciadorMeteoro.cs
+++ Assets/Recursos/Cartas/Carta3/Scripts/InstanciadorMeteoro.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class InstanciadorMeteoro : MonoBehaviour{
 
     public GameObject meteoro;
-
     public Transform pontoFinal;
     public int distanciaMeteoro = 10;
 
@@ -19,12 +18,7 @@ public class InstanciadorMeteoro : MonoBehaviour{
     private void Spawn(){
         Vector3 posicaoInicial = new Vector3(0, ((float)distanciaMeteoro)/10, 0);
         meteoroAtual = Instantiate(meteoro, posicaoInicial, Quaternion.identity) as GameObject;
-        meteoroAtual.transform.SetParent(pontoFinal, false); // setando o meteoro como filho de PontoFinal
-        //Debug.Log(posicaoInicial);
-        //Vector3 posicaoFinal = pontoFinal.transform.position;
-        //Vector3 direction = posicaoFinal - meteoroInstanciado.transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(direction);
-        //meteoroInstanciado.transform.localRotation = Quaternion.Lerp(meteoroInstanciado.transform.rotation, rotation, 1);
+        meteoroAtual.transform.SetParent(pontoFinal, false);
     }
 
     public void colidiu() {
---
+++ Assets/ScriptParticulas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ScriptParticulas : MonoBehaviour{
+
+    private ParticleSystem sp;
+    private ParticleSystem.Particle[] particulas;
+
+    void Start(){
+        sp = GetComponent<ParticleSystem>();
+        particulas = new ParticleSystem.Particle[sp.main.maxParticles];
+    }
+    void Update(){
+        int numParticlesAlive = sp.GetParticles(particulas);
+        Debug.Log(numParticlesAlive);
+
+    }
+}
