babc8bb59bab29c3cb61fa24bfeaaa47a3b2d5fa
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-20 20:54:21 -0300

    ajustes da professora na carta 1 e 2, inserção de clique no botao virtual com touch

--- Assets/Recursos/Botao/ControladorBotaoRA.cs
+++ Assets/Recursos/Botao/ControladorBotaoRA.cs
@@ -6,21 +6,51 @@ using Vuforia;
 public class ControladorBotaoRA : MonoBehaviour, IVirtualButtonEventHandler{
 
     public GameObject[] objetos;
-    public GameObject vuforiaVirtualButton;
-    public GameObject botao3D;
+    public GameObject prefabBotao;
 
     private int index = 0;
 
     void Start() {
-        vuforiaVirtualButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        gameObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                mudarObjeto();
+                animacaoTocar();
+            }
+        }
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                mudarObjeto();
+                animacaoTocar();
+            }
+        }
+    }
+    private void mudarObjeto() {
+        objetos[index].SetActive(false);
+        index = (index == (objetos.Length - 1)) ? 0 : (index + 1);
+        objetos[index].SetActive(true);
     }
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
-        objetos[index].SetActive(false);
-        index = (index == (objetos.Length-1)) ? 0 : (index + 1);
-        objetos[index].SetActive(true);
-        botao3D.GetComponent<Animator>().Play("pressionar");
+        mudarObjeto();
+        animacaoPressionar();
     }
     public void OnButtonReleased(VirtualButtonBehaviour vb) {
-        botao3D.GetComponent<Animator>().Play("soltar");
+        animacaoSoltar();
+    }
+
+    private void animacaoTocar() {
+        prefabBotao.GetComponent<Animator>().Play("tocar");
+    }
+    private void animacaoPressionar() {
+        prefabBotao.GetComponent<Animator>().Play("pressionar");
+    }
+    private void animacaoSoltar() {
+        prefabBotao.GetComponent<Animator>().Play("soltar");
     }
 }
