fb2a72b5940cc3a67d4aa01efb7483e21ca77d3b
Guilherme Lütkemeyer Eyng <guilherme_eyng@hotmail.com>
2019-10-29 03:40:13 -0300

    Rearranjo de pastas

---
+++ Assets/Scripts/Componentes/ControladorJogo.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorJogo : MonoBehaviour{
+
+    /*
+     * vetor para os peões usados como targets no vuforia,
+     * inseridos atraves do unity
+     */
+    public GameObject[] peoes;
+
+    /*
+     * vetor para os dinossauros usados como personagens
+     * atrelados aos peões dinamicamente apos a seleção
+     * do menu principal
+     */
+    public GameObject[] personagens;
+
+    /*
+     * Assim que a classe é instanciada, as variáveis de seleção
+     * do dino e do peão é carregada, e atrelado, para que possa 
+     * ser reconhecido pelo vuforia
+     */
+    void Start() {
+        if (personagens.Length > 0 && peoes.Length > 0) {
+            int indexPersonagem = ControladorCenas.getParametro(ControladorCenas.PERSONAGEM);
+            int indexPeao = ControladorCenas.getParametro(ControladorCenas.PEAO);
+            personagens[indexPersonagem].transform.parent = peoes[indexPeao].transform;
+        }
+    }
+    /*
+     * Caso clique no botão de voltar, carrega a cena do menu
+     * de seleção
+     */
+    public void OnClickVoltar() {
+        GameObject pnDinossauro = GameObject.FindGameObjectWithTag(TagsUI.CenaJogo.PnDinossauro);
+        if (pnDinossauro.GetComponent<AnimadorAbrirFechar>().isAberto()) {
+            pnDinossauro.GetComponent<AnimadorAbrirFechar>().fechar();
+        } else {
+            ControladorCenas.carregaCena(ControladorCenas.CENA_MENU);
+        }
+    }
+
+    /*
+     * Se clicar no botão de abrir/fechar o painel de instruções,
+     * acessa o controlador de estados do botao, e se houver, faz
+     * a mudança de estado, e acessa o painel alterando sua visibilidade
+     */
+    public void OnClickInstrucao() {
+        GameObject PnDinossauro = GameObject.FindGameObjectWithTag(TagsUI.CenaJogo.PnDinossauro);
+        if(PnDinossauro != null) {
+            if (!PnDinossauro.GetComponent<AnimadorAbrirFechar>().isAberto()) {
+                GameObject btnInstrucao = GameObject.FindWithTag(TagsUI.CenaJogo.BtnInstrucao);
+                if (btnInstrucao != null) {
+                    ControladorEstadosBotao controladorEstadosBotao = btnInstrucao.GetComponent<ControladorEstadosBotao>();
+                    if (controladorEstadosBotao != null) {
+                        controladorEstadosBotao.mudarEstado();
+                    }
+                }
+                GameObject pnInstrucao = GameObject.FindGameObjectWithTag(TagsUI.CenaJogo.PnInstrucao);
+                if (pnInstrucao != null) {
+                    pnInstrucao.GetComponent<AnimadorAbrirFechar>().abrirOuFechar();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objetos/TagsUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagsUI{
+    public class CenaMenu{
+        public const string BtnInstrucao = "BtnSelecionar";
+        public const string BtnVoltar = "BtnVoltar";
+    }
+    public class CenaJogo{
+        public const string BtnInstrucao = "BtnInstrucao";
+        public const string BtnVoltar = "BtnVoltar";
+
+        public const string PnDinossauro = "PnDinossauro";
+        public const string ImgDinossauro = "ImgDinossauro";
+        public const string TxtGenero = "TxtGenero";
+        public const string TxtFamilia = "TxtFamilia";
+        public const string TxtDescricao = "TxtDescricao";
+
+        public const string PnInstrucao = "PnInstrucao";
+    }
+    
+}
